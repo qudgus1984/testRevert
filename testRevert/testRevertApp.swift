e61ddc11da1d551adf2850e0885eefe0ec02e942
cody <qudgus1984@naver.com>
2022-12-26 20:22:18 +0900

    Initial Commit

---
+++ testRevert/testRevertApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRevertApp.swift
+//  testRevert
+//
+//  Created by 이병현 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct testRevertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
